750a22d7a39682a3f298032794f5f8ab7a56e375
Shahed Khan <63577807+mdshahedkhan@users.noreply.github.com>
2025-01-16 14:06:36 +0600

    Fix Connection DB

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,27 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
 const MONGO_URI = process.env.MONGO_URI || '';
 
 if (!MONGO_URI) {
-  throw new Error('Please define the MONGO_URI environment variable');
+    throw new Error('Please define the MONGO_URI environment variable');
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
 
 const connectMongo = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => mongoose);
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+    const connection = mongoose.connect(MONGO_URI).then((mongoose) => mongoose);
+
+    return (await connection).Connection;
 };
 
 export default connectMongo;
